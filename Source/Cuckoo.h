53ad1d9b81c412e3d85ebf5a9d7784b4d87b4c05
Tadeas Kriz <tadeas@brightify.org>
2016-01-19 15:59:31 +0100

    Remove UIKit dependency.

--- Source/Cuckoo.h
+++ Source/Cuckoo.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Brightify. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Cuckoo.
 FOUNDATION_EXPORT double CuckooVersionNumber;
