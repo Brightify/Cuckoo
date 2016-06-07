598be229672025bf1d77b4ad25f7bfd451297550
Tadeas Kriz <tadeas@brightify.org>
2016-06-07 20:57:15 +0200

    Fix description texts from last commit.

--- Tests/MatcherDescriptionsTest.swift
+++ Tests/MatcherDescriptionsTest.swift
@@ -23,27 +23,27 @@ class MatcherDescriptionsTest: XCTestCase {
         equalTo(10).describeTo(stringDescription)
         equalTo(10).describeMismatch(20, to: stringDescription)
 
-        XCTAssertEqual(stringDescription.description, "Expected to be equal to <10> was <20>")
+        XCTAssertEqual(stringDescription.description, "to be equal to <10> was <20>")
     }
 
     func testCalledTimes() {
         times(10).describeTo(stringDescription)
         times(10).describeMismatch([], to: stringDescription)
 
-        XCTAssertEqual(stringDescription.description, "Expected to be called <10> times was called <0> times")
+        XCTAssertEqual(stringDescription.description, "to be called <10> times was called <0> times")
     }
 
     func testCalledAtLest() {
         atLeast(10).describeTo(stringDescription)
         atLeast(10).describeMismatch([], to: stringDescription)
 
-        XCTAssertEqual(stringDescription.description, "Expected to be called at least <10> times was called <0> times")
+        XCTAssertEqual(stringDescription.description, "to be called at least <10> times was called <0> times")
     }
 
     func testCalledAtMost() {
         atMost(10).describeTo(stringDescription)
         atMost(10).describeMismatch([], to: stringDescription)
 
-        XCTAssertEqual(stringDescription.description, "Expected to be called at most <10> times was called <0> times")
+        XCTAssertEqual(stringDescription.description, "to be called at most <10> times was called <0> times")
     }
 }
