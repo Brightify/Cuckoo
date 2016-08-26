dde17799c8b78d4e47e4c2e08a1963ef471b2d46
Filip Dolník <fdolnik@volny.cz>
2016-08-26 14:15:56 +0200

    Refactoring.

--- Generator/Source/GenerateMocksCommand.swift
+++ Generator/Source/GenerateMocksCommand.swift
@@ -12,6 +12,11 @@ import SourceKittenFramework
 import FileKit
 import CuckooGeneratorFramework
 
+private func curry<P1, P2, P3, P4, P5, P6, R>(f: (P1, P2, P3, P4, P5, P6) -> R)
+    -> (P1) -> (P2) -> (P3) -> (P4) -> (P5) -> (P6) -> R {
+        return { p1 in { p2 in { p3 in { p4 in { p5 in { p6 in f(p1, p2, p3, p4, p5, p6) } } } } } }
+}
+
 public struct GenerateMocksCommand: CommandType {
     
     public let verb = "generate"
@@ -55,12 +60,17 @@ public struct GenerateMocksCommand: CommandType {
         let testableFrameworks: [String]
         let filePrefix: String
         
-        public static func create(output: String)(testableFrameworks: String)(noHeader: Bool)(noTimestamp: Bool)(filePrefix: String)(files: [String]) -> Options {
-            return Options(files: files, output: output, noHeader: noHeader, noTimestamp: noTimestamp, testableFrameworks: testableFrameworks.componentsSeparatedByString(",").filter { !$0.isEmpty }, filePrefix: filePrefix)
+        public init(output: String, testableFrameworks: String, noHeader: Bool, noTimestamp: Bool, filePrefix: String, files: [String]) {
+            self.output = output
+            self.testableFrameworks = testableFrameworks.componentsSeparatedByString(",").filter { !$0.isEmpty }
+            self.noHeader = noHeader
+            self.noTimestamp = noTimestamp
+            self.filePrefix = filePrefix
+            self.files = files
         }
         
         public static func evaluate(m: CommandMode) -> Result<Options, CommandantError<CuckooGeneratorError>> {
-            return create
+            return curry(Options.init)
                 <*> m <| Option(key: "output", defaultValue: "GeneratedMocks.swift", usage: "Where to put the generated mocks.\nIf a path to a directory is supplied, each input file will have a respective output file with mocks.\nIf a path to a Swift file is supplied, all mocks will be in a single file.\nDefault value is `GeneratedMocks.swift`.")
                 <*> m <| Option(key: "testable", defaultValue: "", usage: "A comma separated list of frameworks that should be imported as @testable in the mock files.")
                 <*> m <| Option(key: "no-header", defaultValue: false, usage: "Do not generate file headers.")
