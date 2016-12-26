6c7bc91cb54d7d2ba7d87acc929b260a0ce65ed2
Filip Dolník <filip.dolnik.96@gmail.com>
2016-12-26 13:36:00 +0100

    Fix run script.

--- Generator/record_tests.sh
+++ Generator/record_tests.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-export TOOLCHAINS=com.apple.dt.toolchain.Swift_2_3
 ./run_tests.sh "$@"
-for file in Build/log/*/*.swift; do
-	fileName=$(echo "$file" | rev | cut -d"/" -f1 | rev)
-	expectedFile="Tests/SourceFiles/Expected/${fileName}"
-	if [ -f $expectedFile ]; then
-		cp "$file" $expectedFile
+for file in Build/log/*/*.swift; do 
+	fileName="$(echo "$file" | awk -F"/" '{print $NF}')"
+	expectedFile="Tests/SourceFiles/Expected/$fileName"
+	if [ -f "$expectedFile" ]; then
+		cp "$file" "$expectedFile"
 	fi
 done
--- Generator/run_tests.sh
+++ Generator/run_tests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-export TOOLCHAINS=com.apple.dt.toolchain.Swift_2_3
-if [ "$1" != "--no-build" ]; then
+FILE_NAME="cuckoo_generator.app"
+if [[ "$1" == "--clean" || ! -d "$FILE_NAME" ]]; then
 	rm -rf Build
 	mkdir Build
-	xcodebuild -project 'CuckooGenerator.xcodeproj' -scheme 'CuckooGenerator' -configuration 'Release' CONFIGURATION_BUILD_DIR=$(pwd)/Build clean build
+	xcodebuild -project 'CuckooGenerator.xcodeproj' -scheme 'CuckooGenerator' -configuration 'Release' CONFIGURATION_BUILD_DIR="$PWD/Build" clean build
 fi
 cd Tests
 cucumber
