349fa1c15483330b49557546b88b2acc307c396f
Matyáš Kříž <matyas@brightify.org>
2019-01-31 13:24:26 +0100

    Temporarily fix unintentional collision because of fail-safe classes being in the input file.
    It creates way too many merge conflicts if we tried to apply the full fix here.

--- Tests/Source/TestedClass.swift
+++ Tests/Source/TestedClass.swift
@@ -202,5 +202,5 @@ final class FinalClass {
 }
 
 // should generate a compiler error if `FinalClass` isn't ignored and `MockFinalClass` is generated
-class MockFinalClass {}
-class FinalClassStub {}
+final class MockFinalClass {}
+final class FinalClassStub {}
