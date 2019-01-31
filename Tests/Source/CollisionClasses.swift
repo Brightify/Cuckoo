807dc8953b41bce4e82496eb8e3c96e30bec583e
Matyáš Kříž <matyas@brightify.org>
2019-01-31 12:55:59 +0100

    Move collision classes to different file, so that they don’t get mocked.

---
+++ Tests/Source/CollisionClasses.swift
@@ -0,0 +1,12 @@
+//
+//  CollisionClasses.swift
+//  Cuckoo
+//
+//  Created by Matyáš Kříž on 31/01/2019.
+//
+
+import Foundation
+
+// should generate a compiler error if `FinalClass` isn't ignored and `MockFinalClass` is generated
+class MockFinalClass {}
+class FinalClassStub {}
