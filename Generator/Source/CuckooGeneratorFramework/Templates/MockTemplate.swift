8a1fa6c8d99631c5d0616d608501a35c24061f0f
Pubbus <lephihungch@gmail.com>
2019-10-24 00:33:47 +0700

    Fix closure generation, return type is required in `withoutActuallyEscaping` from swift 5.1 (XCode11)

--- Generator/Source/CuckooGeneratorFramework/Templates/MockTemplate.swift
+++ Generator/Source/CuckooGeneratorFramework/Templates/MockTemplate.swift
@@ -103,7 +103,7 @@ extension Templates {
     {{ attribute.text }}
     {% endfor %}
     {{ method.accessibility }}{% if container.isImplementation and method.isOverriding %} override{% endif %} func {{ method.name }}{{ method.genericParameters }}({{ method.parameterSignature }}) {{ method.returnSignature }} {
-        {{ method.parameters|openNestedClosure:method.isThrowing }}
+        {{ method.parameters|openNestedClosure:method.isThrowing, :method.returnType }}
     return{% if method.isThrowing %} try{% endif %} cuckoo_manager.call{% if method.isThrowing %}{{ method.throwType|capitalize }}{% endif %}("{{method.fullyQualifiedName}}",
             parameters: ({{method.parameterNames}}),
             escapingParameters: ({{method.escapingParameterNames}}),
