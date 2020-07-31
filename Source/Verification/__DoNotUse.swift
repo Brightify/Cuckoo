403b225770d781824e855e29306378adfe53d853
Mike Gray <mgray88@gmail.com>
2020-07-31 10:26:49 -0400

    Allow verify to specify return type for generic functions

--- Source/Verification/__DoNotUse.swift
+++ Source/Verification/__DoNotUse.swift
@@ -8,3 +8,10 @@
 
 /// Marker struct for use as a return type in verification.
 public struct __DoNotUse<IN, OUT> { }
+
+public extension __DoNotUse {
+
+    /// Hint at the return type when verifying a function where the function is generic
+    /// and the type is inferred from the return value
+    func with(returnType: OUT.Type) { }
+}
