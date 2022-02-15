e4a015348574e4b4d75264098b5ed1d901d742a2
Antonio J Pallares <ajpallares@gmail.com>
2022-02-15 13:15:42 +0100

    Add SPM support for Cuckoo+OCMock.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
@@ -8,12 +8,30 @@ let package = Package(
         .library(
             name: "Cuckoo",
             targets: ["Cuckoo"]),
+        .library(
+            name: "Cuckoo_OCMock",
+            targets: ["Cuckoo", "Cuckoo_OCMock"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/erikdoe/ocmock",
+            .revisionItem("afd2c6924e8a36cb872bc475248b978f743c6050")
+        )
     ],
     targets: [
         .target(
             name: "Cuckoo",
             dependencies: [],
             path: "Source"),
+        .target(
+            name: "Cuckoo_OCMock-Objc",
+            dependencies: ["OCMock"],
+            path: "OCMock/ObjCHelpers",
+            publicHeadersPath: "."),
+        .target(
+            name: "Cuckoo_OCMock",
+            dependencies: ["Cuckoo", "Cuckoo_OCMock-Objc"],
+            path: "OCMock/Swift"),
         .testTarget(
             name: "CuckooTests",
             dependencies: ["Cuckoo"],
