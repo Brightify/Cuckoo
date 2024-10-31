225f873aaac331f53c6cd93b7b6bbca8b84244b6
Austin Rovge <austinrovge@gmail.com>
2024-10-30 20:15:41 -0500

    add swift-testing variants of StubTest tests

--- Tests/Swift/StubTest.swift
+++ Tests/Swift/StubTest.swift
@@ -1,5 +1,6 @@
-import XCTest
 import Cuckoo
+import Testing
+import XCTest
 @testable import CuckooMocks
 
 final class StubTest: XCTestCase {
@@ -81,6 +82,100 @@ final class StubTest: XCTestCase {
         XCTAssertEqual([:], stub.dictionaryFunction())
     }
 
+    func testVoidFunction() {
+        // Test for crash
+        stub.voidFunction()
+    }
+}
+
+struct StubSwiftTestingTests {
+    let stub = ClassForStubTestingStub()
+
+    @Test
+    func intProperty() {
+        stub.intProperty = 1
+        #expect(stub.intProperty == 0)
+    }
+
+    @Test
+    func arrayProperty() {
+        stub.arrayProperty = [1]
+        #expect(stub.arrayProperty == [])
+    }
+
+    @Test
+    func setProperty() {
+        stub.setProperty = Set([1])
+        #expect(stub.setProperty == Set())
+    }
+
+    @Test
+    func dictionaryProperty() {
+        stub.dictionaryProperty = ["a": 1]
+        #expect(stub.dictionaryProperty == [:])
+    }
+
+    @Test
+    func tuple1() {
+        stub.tuple1 = (2)
+        #expect(stub.tuple1 == (0))
+    }
+
+    @Test
+    func tuple2() {
+        stub.tuple2 = (2, 1)
+        #expect(stub.tuple2 == (0, 0))
+    }
+
+    @Test
+    func tuple3() {
+        stub.tuple3 = (2, 1, true)
+        #expect(stub.tuple3 == (0, 0, false))
+    }
+
+    @Test
+    func tuple4() {
+        stub.tuple4 = (2, 1, 1, 1)
+        #expect(stub.tuple4 == (0, 0, 0, 0))
+    }
+
+    @Test
+    func tuple5() {
+        stub.tuple5 = (2, 1, 1, 1, 1)
+        #expect(stub.tuple5 == (0, 0, 0, 0, 0))
+    }
+
+    @Test
+    func tuple6() {
+        stub.tuple6 = (2, "A", 1, 1, 1, 1)
+        #expect(stub.tuple6 == (0, "", 0, 0, 0, 0))
+    }
+
+    @Test
+    func intFunction() {
+        stub.intProperty = 1
+        #expect(stub.intFunction() == 0)
+    }
+
+    @Test
+    func arrayFunction() {
+        stub.arrayProperty = [1]
+        #expect(stub.arrayFunction() == [])
+    }
+
+    @Test
+    func setFunction() {
+        stub.setProperty = Set([1])
+        #expect(stub.setFunction() == Set())
+    }
+
+    @Test
+    func dictionaryFunction() {
+        stub.dictionaryProperty = ["a": 1]
+        #expect(stub.dictionaryFunction() == [:])
+    }
+
+    @Test
     func testVoidFunction() {
         // Test for crash
         stub.voidFunction()
