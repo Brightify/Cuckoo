86037462c3219de89c4b3b907ed77ecd5b9b1e64
Vincent Guerci <vincent.guerci@onfido.com>
2025-03-13 15:37:25 +0100

    Fix swiftlang dependencies

--- Generator/Project.swift
+++ Generator/Project.swift
@@ -44,8 +44,8 @@ let project = Project(
         // Any dependency changes must also be reflected in ../Package.swift.
         .package(url: "https://github.com/nvzqz/FileKit.git", .exact("6.1.0")),
         .package(url: "https://github.com/kylef/Stencil.git", .exact("0.15.1")),
-        .package(url: "https://github.com/apple/swift-syntax.git", .exact("509.0.0")),
-        .package(url: "https://github.com/apple/swift-format.git", .exact("509.0.0")),
+        .package(url: "https://github.com/swiftlang/swift-syntax", .exact("509.0.0")),
+        .package(url: "https://github.com/swiftlang/swift-format", .exact("509.0.0")),
         .package(url: "https://github.com/apple/swift-argument-parser", .exact("1.2.3")),
         .package(url: "https://github.com/LebJe/TOMLKit.git", .exact("0.5.5")),
         .package(url: "https://github.com/tuist/XcodeProj.git", .exact("8.15.0")),
